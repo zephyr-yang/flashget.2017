59ef5156c20c1be9489c0d5ed24ace2d0bd49852
zephyr yang <1867818+zephyr-yang@users.noreply.github.com>
2017-07-31 00:34:54 -0400

    Format the source code.

--- client_uri.c
+++ client_uri.c
@@ -1,109 +1,109 @@
 #include <stdio.h>
 
-int 
-extractHostFromUri(char *resourceUri, char *host, int size){
+int
+extractHostFromUri(char *resourceUri, char *host, int size) {
 
-	if(resourceUri != NULL && host != NULL){
+    if(resourceUri != NULL && host != NULL) {
 
-		char *ptr = resourceUri;
-		
-		int len = 0;
+        char *ptr = resourceUri;
 
-		while( *ptr != '\0' && len < size){
+        int len = 0;
 
-			int offset = ptr -resourceUri;
+        while( *ptr != '\0' && len < size) {
 
-			if(*ptr != '/' && offset > 6){
+            int offset = ptr -resourceUri;
 
-				host[len++] = *ptr; 
-			} 			
+            if(*ptr != '/' && offset > 6) {
 
-			if(*ptr == '/' && (ptr-resourceUri) > 7){
-
-				host[len] = '\0'; 
-
-				break; 
-			}
-		
-			ptr++;
-		}
-	}
-
-	return 0;	
-}
-
-int 
-extractPathFromUri(char *resourceUri, char *path, int size){
-
-	if(resourceUri != NULL && path != NULL){
-
-		char *ptr = resourceUri;
-		
-		int len = 0;
-
-        int findPath = 0;
-
-		while( *ptr != '\0' && len < size){
-
-			int offset = ptr -resourceUri;
-
-			if(findPath == 1){
-
-				path[len++] = *ptr; 
-			} 			
-
-			if(*ptr == '/' && (ptr-resourceUri) > 7 && findPath == 0){
-
-				path[len++] = *ptr; 
-                
-                findPath = 1;
-			}
-		
-			ptr++;
-		}
-
-        if(len < size){
-            path[len] = '\0';
-        }else{
-            path[size-1] = '\0';
-        }  
-	}
-
-	return 0;	
-}
-
-int 
-extractFilenameFromUri(char *resourceUri, char *filename, int size){
-
-	if(resourceUri != NULL && filename != NULL){
-
- 		char *ptr = resourceUri;
-
-		int len = 0;
-
-        int findFile = 0;
-
-		while(*ptr != '\0' && len < size){
-
-			if(findFile == 1){
-				filename[len++] = *ptr;
+                host[len++] = *ptr;
             }
-			
-			if(*ptr == '/'){
-				findFile = 1;				
-				len =0;
+
+            if(*ptr == '/' && (ptr-resourceUri) > 7) {
+
+                host[len] = '\0';
+
+                break;
             }
 
             ptr++;
-		}       
+        }
+    }
 
-        if(len < size){
+    return 0;
+}
+
+int
+extractPathFromUri(char *resourceUri, char *path, int size) {
+
+    if(resourceUri != NULL && path != NULL) {
+
+        char *ptr = resourceUri;
+
+        int len = 0;
+
+        int findPath = 0;
+
+        while( *ptr != '\0' && len < size) {
+
+            int offset = ptr -resourceUri;
+
+            if(findPath == 1) {
+
+                path[len++] = *ptr;
+            }
+
+            if(*ptr == '/' && (ptr-resourceUri) > 7 && findPath == 0) {
+
+                path[len++] = *ptr;
+
+                findPath = 1;
+            }
+
+            ptr++;
+        }
+
+        if(len < size) {
+            path[len] = '\0';
+        } else {
+            path[size-1] = '\0';
+        }
+    }
+
+    return 0;
+}
+
+int
+extractFilenameFromUri(char *resourceUri, char *filename, int size) {
+
+    if(resourceUri != NULL && filename != NULL) {
+
+        char *ptr = resourceUri;
+
+        int len = 0;
+
+        int findFile = 0;
+
+        while(*ptr != '\0' && len < size) {
+
+            if(findFile == 1) {
+                filename[len++] = *ptr;
+            }
+
+            if(*ptr == '/') {
+                findFile = 1;
+                len =0;
+            }
+
+            ptr++;
+        }
+
+        if(len < size) {
             filename[len] = '\0';
-        }else{
+        } else {
             filename[size-1] = '\0';
-        }  
-	}
+        }
+    }
 
-	return 0;	
+    return 0;
 }
 
